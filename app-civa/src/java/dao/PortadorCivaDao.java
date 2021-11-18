a643508f6375441488618bd2daf00bc581c892be
randelsouza <randelsouza88@gmail.com>
2021-11-18 11:34:12 -0300

    PotadorDao criado por Otacilio Lima

--- app-civa/src/java/dao/PortadorCivaDao.java
+++ app-civa/src/java/dao/PortadorCivaDao.java
@@ -1,9 +1,219 @@
 package dao;
 
+import java.util.ArrayList;
+import java.util.List;
+import model.Acessopc;
+import model.Docs;
+import model.Endereco;
+import model.Pessoa;
+import model.PortadorCiva;
+
 /**
  *
- * @author randel
+ * @author Otacilio Lima
  */
 public class PortadorCivaDao {
-    
+
+    public static boolean insert(PortadorCiva portadorciva) {
+        boolean resultado = false;
+
+        // Insert into PortadorCIVA values (?, ?, ?, ?);
+        if (true) {
+            // se conseguiu inserir no banco
+            resultado = true;
+        }
+
+        return resultado;
+    }
+
+    public static PortadorCiva find(String nomePortadorCiva) {
+        PortadorCiva portadorciva = new PortadorCiva();
+
+        Pessoa pessoa = new Pessoa();
+        pessoa.setNome("");
+        pessoa.setSobrenome("");
+        pessoa.setDataNascimento("");
+        pessoa.setTelefoneDdd("");
+        pessoa.setGenero("");
+        pessoa.setIdNacionalidade(1);
+        pessoa.setIdPessoa(1);
+
+        Acessopc acessopc = new Acessopc();
+        acessopc.setCodigoCiva(1);
+        acessopc.setEmail("");
+        acessopc.setSenha("");
+        // acessopc.setIdCadastrante(1);
+
+        Docs documento1 = new Docs();
+        documento1.setDataEmissao("");
+        documento1.setDocumento("");
+        documento1.setIdPessoa(1);
+        documento1.setIddocs(1);
+        documento1.setIdtipodoc(1);
+
+        Docs documento2 = new Docs();
+        documento2.setDataEmissao("");
+        documento2.setDocumento("");
+        documento2.setIdPessoa(1);
+        documento2.setIddocs(1);
+        documento2.setIdtipodoc(1);
+
+        Docs documento3 = new Docs();
+        documento3.setDataEmissao("");
+        documento3.setDocumento("");
+        documento3.setIdPessoa(1);
+        documento3.setIddocs(1);
+        documento3.setIdtipodoc(1);
+
+        Endereco endereco = new Endereco();
+        endereco.setIdEndereco(1);
+        endereco.setIdPais(1);
+        endereco.setTipoLogradouro("");
+        endereco.setLogradouro("");
+        endereco.setCodigoPostal("");
+        endereco.setNomesubdivisao1("");
+        endereco.setNomesubdivisao2("");
+        endereco.setNomesubdivisao3("");
+        endereco.setNomesubdivisao4("");
+        endereco.setNomesubdivisao5("");
+        endereco.setNomesubdivisao6("");
+        endereco.setNomesubdivisao7("");
+        // SELECT * FROM portadorciva where nome="";
+
+        portadorciva.setPessoa(pessoa);
+        portadorciva.setAcessopc(acessopc);
+        portadorciva.setDocumento1(documento1);
+        portadorciva.setDocumento2(documento2);
+        portadorciva.setDocumento3(documento3);
+        portadorciva.setEndereco(endereco);
+
+        return portadorciva;
+    }
+
+    public static List<PortadorCiva> list() {
+        List<PortadorCiva> portadoresciva = new ArrayList<PortadorCiva>();
+
+        // SELECT * FROM PortadorCiva;
+        PortadorCiva portadorciva = new PortadorCiva();
+
+        Pessoa pessoa = new Pessoa();
+        pessoa.setNome("");
+        pessoa.setSobrenome("");
+        pessoa.setDataNascimento("");
+        pessoa.setTelefoneDdd("");
+        pessoa.setDdiContato("");
+        pessoa.setGenero("");
+        pessoa.setIdNacionalidade(1);
+        pessoa.setIdPessoa(1);
+
+        Docs documento1 = new Docs();
+        documento1.setDataEmissao("");
+        documento1.setDocumento("");
+        documento1.setIdPessoa(1);
+        documento1.setIddocs(1);
+        documento1.setIdtipodoc(1);
+
+        Docs documento2 = new Docs();
+        documento2.setDataEmissao("");
+        documento2.setDocumento("");
+        documento2.setIdPessoa(1);
+        documento2.setIddocs(1);
+        documento2.setIdtipodoc(1);
+
+        Docs documento3 = new Docs();
+        documento3.setDataEmissao("");
+        documento3.setDocumento("");
+        documento3.setIdPessoa(1);
+        documento3.setIddocs(1);
+        documento3.setIdtipodoc(1);
+
+        Endereco endereco = new Endereco();
+        endereco.setCodigoPostal("");
+        endereco.setIdEndereco(1);
+        endereco.setIdPais(1);
+        endereco.setLogradouro("");
+        endereco.setNomesubdivisao1("");
+        endereco.setNomesubdivisao2("");
+        endereco.setNomesubdivisao3("");
+        endereco.setNomesubdivisao4("");
+        endereco.setNomesubdivisao5("");
+        endereco.setNomesubdivisao6("");
+        endereco.setNomesubdivisao7("");
+        endereco.setTipoLogradouro("");
+
+        portadoresciva.add(portadorciva);
+
+        PortadorCiva portadorciva2 = new PortadorCiva();
+
+        Pessoa pessoa2 = new Pessoa();
+        pessoa2.setNome("");
+        pessoa2.setSobrenome("");
+        pessoa2.setDataNascimento("");
+        pessoa2.setTelefoneDdd("");
+        pessoa2.setDdiContato("");
+        pessoa2.setGenero("");
+        pessoa2.setIdNacionalidade(1);
+        pessoa2.setIdPessoa(1);
+
+        Docs documento12 = new Docs();
+        documento12.setDataEmissao("");
+        documento12.setDocumento("");
+        documento12.setIdPessoa(1);
+        documento12.setIddocs(1);
+        documento12.setIdtipodoc(1);
+
+        Docs documento22 = new Docs();
+        documento22.setDataEmissao("");
+        documento22.setDocumento("");
+        documento22.setIdPessoa(1);
+        documento22.setIddocs(1);
+        documento22.setIdtipodoc(1);
+
+        Docs documento32 = new Docs();
+        documento32.setDataEmissao("");
+        documento32.setDocumento("");
+        documento32.setIdPessoa(1);
+        documento32.setIddocs(1);
+        documento32.setIdtipodoc(1);
+
+        Endereco endereco2 = new Endereco();
+        endereco2.setCodigoPostal("");
+        endereco2.setIdEndereco(1);
+        endereco2.setIdPais(1);
+        endereco2.setLogradouro("");
+        endereco2.setNomesubdivisao1("");
+        endereco2.setNomesubdivisao2("");
+        endereco2.setNomesubdivisao3("");
+        endereco2.setNomesubdivisao4("");
+        endereco2.setNomesubdivisao5("");
+        endereco2.setNomesubdivisao6("");
+        endereco2.setNomesubdivisao7("");
+        endereco2.setTipoLogradouro("");
+
+        portadoresciva.add(portadorciva2);
+
+        return portadoresciva;
+    }
+
+    public static boolean update(PortadorCiva portadorcivaNovo) {
+        boolean resultado = false;
+
+        // Update portadorciva;
+        if (true) {
+            resultado = true;
+        }
+
+        return resultado;
+    }
+
+    public static boolean delete(PortadorCiva portadorciva) {
+        boolean resultado = false;
+
+        // Delete portadorciva
+        if (true) {
+            resultado = true;
+        }
+
+        return resultado;
+    }
 }
--- app-civa/src/java/model/PortadorCiva.java
+++ app-civa/src/java/model/PortadorCiva.java
@@ -5,6 +5,11 @@ package model;
  */
 public class PortadorCiva {
     private Pessoa pessoa;
+    private Docs documento1;
+    private Docs documento2;
+    private Docs documento3;
+    private Endereco endereco;
+    private Acessopc acessopc;
     private Vacinacao vacinacao;
 
     public Pessoa getPessoa() {
@@ -22,7 +27,45 @@ public class PortadorCiva {
     public void setVacinacao(Vacinacao vacinacao) {
         this.vacinacao = vacinacao;
     }
-    
-    
+
+    public Docs getDocumento1() {
+        return documento1;
+    }
+
+    public void setDocumento1(Docs documento1) {
+        this.documento1 = documento1;
+    }
+
+    public Docs getDocumento2() {
+        return documento2;
+    }
+
+    public void setDocumento2(Docs documento2) {
+        this.documento2 = documento2;
+    }
+
+    public Docs getDocumento3() {
+        return documento3;
+    }
+
+    public void setDocumento3(Docs documento3) {
+        this.documento3 = documento3;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public Acessopc getAcessopc() {
+        return acessopc;
+    }
+
+    public void setAcessopc(Acessopc acessopc) {
+        this.acessopc = acessopc;
+    }
     
 }
