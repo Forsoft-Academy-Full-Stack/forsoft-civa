e05646a7eb1f17c95375520a7beee85f6934222e
randelsouza <randelsouza88@gmail.com>
2021-12-08 17:29:14 -0300

    Update vincular-profissional-saude.js

--- app-civa/web/public/assets/js/supervisor/vincular-profissional-saude.js
+++ app-civa/web/public/assets/js/supervisor/vincular-profissional-saude.js
@@ -10,7 +10,8 @@ let campos_confirmar_senha = ["senha-confirmacao"];
 let modal_confirmar_senha = $("#modal-confirmar-senha");
 
 $("#vincular").click(() => {
-
+    modal_confirmar_senha.modal("show");
+    
     $("#confirmar").click(() => {
         if (tratar_campos(campos_confirmar_senha)) {
             modal_confirmar_senha.modal("hide");
@@ -33,41 +34,7 @@ $("#vincular").click(() => {
             text = 'Pesquise o c&oacute;digo do Profissional desejado';
             swalAlertError(title, text, callback);
         }
-    });
-    if (tratar_campos(campos)) {
-
-        modal_confirmar_senha.modal("show");
-
-        $("#confirmar").click(() => {
-            if (tratar_campos(campos_confirmar_senha)) {
-                modal_confirmar_senha.modal("hide");
-
-                $.post("/app-civa/portador", form.serialize(), (data, status, jqXHR) => {
-                    console.log("Data: " + data.responseData + ", Status: " + status + ", jqXHR: " + jqXHR);
-                    if (status === 'success') {
-                        title = 'Profissional vinculado com sucesso!';
-                        text = "V&iacute;nculo realizado.";
-                        swalAlertSuccess(title, text, callback);
-
-                    }
-                }).fail(function (jqxhr, settings, ex) {
-                    title = 'Erro!';
-                    text = `Algum erro ocorreu e seus dados n&atilde;o foram enviados. Status: ${settings} ${ex}`;
-                    swalAlertError(title, text, callback);
-                });
-            } else {
-                title = 'Senha n&atilde;o preenchida';
-                text = 'Digite a senha do profissional selecionado';
-                swalAlertError(title, text, callback);
-            }
-        });
-
-    } else {
-        title = 'Profissional n&atilde;o pesquisado';
-        text = 'Pesquise o c&oacute;digo do Profissional desejado';
-        swalAlertError(title, text, callback);
-    }
-
+    });         
 });
 
 
